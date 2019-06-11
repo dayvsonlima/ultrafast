303073cba6b190da0c008f07fcd55c3a5d019977
Dayvson Lima <dayvsonlima31@gmail.com>
2019-06-11 16:12:01 -0300

    Add to_i to redis db config

--- lib/ultrafast/storage.rb
+++ lib/ultrafast/storage.rb
@@ -4,7 +4,7 @@ module Ultrafast
       $redis_connection ||= Redis.new(
         host: ENV['UF_REDIS_HOST'] || '127.0.0.1',
         port: ENV['UF_REDIS_PORT'] || 6379,
-        db: ENV['UF_REDIS_DB'] || 0
+        db: (ENV['UF_REDIS_DB'] || 0).to_i
       )
     end
   end
